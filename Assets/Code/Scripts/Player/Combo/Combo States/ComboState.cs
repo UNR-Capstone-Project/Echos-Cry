5c44d7f95dcf2c56b6f30b97a8a831c77ddfad01
satireiguess <cgram79@yahoo.com>
2025-10-03 00:27:55 -0700

    Added ComboStateMachine & ComboState
    -ComboStateMachine will be responsible for combo system behavior
    
    -ComboState is a ScriptableObject that will provide the data related to the combo represents and stores references to the next ComboState's in the current combo branch

---
+++ Assets/Code/Scripts/Player/Combo/Combo States/ComboState.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEditor;
+using Unity.VisualScripting;
+
+[CreateAssetMenu(menuName = "Combo System/Combo State")]
+public class ComboState : ScriptableObject
+{
+    public enum AttackInput
+    {
+        UNASSIGNED = 0,
+        LIGHT_ATTACK,
+        HEAVY_ATTACK
+    }
+    public enum StateType
+    {
+        UNASSIGNED = 0,
+        START_STATE,
+        COMBO_STATE
+    }
+
+    public AttackInput ComboAttackInput;
+    public StateType ComboStateType;
+
+    public ComboState StartState;
+    public ComboState NextLightAttack;
+    public ComboState NextHeavyAttack;
+    
+}
+
+[CustomEditor(typeof(ComboState))]
+public class ComboStateCustomInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        ComboState comboState = (ComboState)target;
+        if (GUI.changed) {
+            if (comboState.NextHeavyAttack != null)
+            {
+                if (comboState.NextHeavyAttack.ComboAttackInput != ComboState.AttackInput.HEAVY_ATTACK)
+                {
+                    comboState.NextHeavyAttack = null;
+                    Debug.LogError("Only ComboState with a ComboAttackInput of HEAVY_ATTACK can be assigned!");
+                }
+            }
+            if (comboState.NextLightAttack != null)
+            {
+                if (comboState.NextLightAttack.ComboAttackInput != ComboState.AttackInput.LIGHT_ATTACK)
+                {
+                    comboState.NextLightAttack = null;
+                    Debug.LogError("Only ComboState with a ComboAttackInput of LIGHT_ATTACK can be assigned!");
+                }
+            }
+
+            if (comboState.StartState != null)
+            {
+                if(comboState.StartState.ComboStateType != ComboState.StateType.START_STATE)
+                {
+                    comboState.StartState = null;
+                    Debug.LogError("Only a ComboState registered as a START_STATE in ComboStateType can be assigned!");
+                }
+            }
+        }
+    }
+}
