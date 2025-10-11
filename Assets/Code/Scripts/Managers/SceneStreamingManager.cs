959a63cb9861ac48b9adf9773f4c438e358e4895
Hyphenix <itsvictordang@gmail.com>
2025-10-10 17:41:47 -0700

    test push

---
+++ Assets/Code/Scripts/Managers/SceneStreamingManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+/// <summary>
+/// SceneStreamingManager is for loading and unloading parts of the same level asynchronously
+/// </summary>
+public class SceneStreamingManager : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Code/Scripts/Managers/SceneTriggerManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+/// <summary>
+/// SceneTriggerManager is for loading scenes with a clear distinction between each other
+/// eg. loading from a level to a completely different level, or boss arena, etc 
+/// </summary>
+public class SceneTriggerManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
