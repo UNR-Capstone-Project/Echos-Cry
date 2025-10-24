a4de1449a4ee1ef49e0ca68eb8dab523fce9e2b4
Hyphenix <itsvictordang@gmail.com>
2025-10-24 15:46:25 -0700

    further debugging to validate sfxManager can create a sound player

--- Assets/Code/Scripts/sfxTestingScript.cs
+++ Assets/Code/Scripts/sfxTestingScript.cs
@@ -3,18 +3,13 @@ using UnityEngine;
 
 public class sfxTestingScript : MonoBehaviour
 {
-    [SerializeField] soundEffect testSoundA;
     private Vector3 posA = Vector3.zero;
-
-    [SerializeField] soundEffectPlayer playerMethodCheck;
 
     void Start()
     {
+        soundEffectManager.Instance.createSoundPlayer();
         //soundEffectManager.Instance.createSoundPlayer()
         
-        //player can setup a sound and play and stop correctly
-        playerMethodCheck.setupSoundEffect(testSoundA);
-        playerMethodCheck.Play();
     }
 
     
