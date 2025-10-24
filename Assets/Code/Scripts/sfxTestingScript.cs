e9efaf4c9a3caf0b83161b259d02eccd9326940f
Hyphenix <itsvictordang@gmail.com>
2025-10-23 19:31:01 -0700

    file creation of testing scripts

---
+++ Assets/Code/Scripts/sfxTestingScript.cs
@@ -0,0 +1,13 @@
+using AudioSystem;
+using UnityEngine;
+
+public class sfxTestingScript : MonoBehaviour
+{
+    [SerializeField] soundEffect testSoundA;
+    [SerializeField] soundEffect testSoundB;
+    [SerializeField] soundEffect testSoundC;
+    [SerializeField] soundEffect testSoundD;
+
+    
+    
+}
