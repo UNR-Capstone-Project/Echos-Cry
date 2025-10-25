595df910497b0c2c4cfa80043a9e3fb8a1a533e7
Hyphenix <itsvictordang@gmail.com>
2025-10-24 18:17:53 -0700

    ensuring test script works to play either sound, and make sound effects not loop unless is ambience

--- Assets/Code/Scripts/sfxTestingScript.cs
+++ Assets/Code/Scripts/sfxTestingScript.cs
@@ -1,22 +1,50 @@
 using System;
+using System.Collections;
 using AudioSystem;
 using UnityEngine;
-
+using Random = UnityEngine.Random;
 public class sfxTestingScript : MonoBehaviour
 {
     [SerializeField] soundEffect singleSound;
     [SerializeField] soundEffect multiClipSound;
     private Vector3 posA = Vector3.zero;
 
-    void Start()
+    private IEnumerator Start()
     {
-        soundEffectManager.Instance.createSound()
-            .setSound(multiClipSound)
-            .setSoundPosition(posA)
-            .ValidateAndPlaySound();
+        
+        for (int i = 0; i < 2; i++)
+        {
+            int randomSound;
+
+            if (i == 0)
+            {
+                randomSound = 1;
+            } else
+            {
+                randomSound = 2;
+            }
+
+            if (randomSound == 1)
+            {
+                soundEffectManager.Instance.createSound()
+                    .setSound(singleSound)
+                    .setSoundPosition(posA)
+                    .ValidateAndPlaySound();
+                Debug.Log("SOUND A PLAYED");
+            }
+            else
+            {
+                soundEffectManager.Instance.createSound()
+                    .setSound(multiClipSound)
+                    .setSoundPosition(posA)
+                    .ValidateAndPlaySound();
+                Debug.Log("====SECOND SOUND PLAYED====");
+            }
+
+            yield return new WaitForSeconds(3);
+        }
 
         
     }
 
-    
 }
