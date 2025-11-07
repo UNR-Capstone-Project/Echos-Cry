d78921a3c62e8ad9d38f429efbfced4d0e2119a3
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2025-11-06 20:45:46 -0800

    Added finger object that can be collected. Also reorganized scripts that were not being actively used.

---
+++ Assets/Code/Scripts/Items/DroppedItem.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class DroppedItem : MonoBehaviour
+{
+    private GameObject mPlayer;
+    [SerializeField] private float itemSpeed = 4f;
+    [SerializeField] float itemDragDistance = 4f;
+    [SerializeField] Rigidbody itemBody;
+    private bool itemDestroyed = false;
+
+    private void Start()
+    {
+        mPlayer = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        //TODO: limit number of times check is performed to reduce calculations.
+        if (mPlayer != null)
+        {
+            float playerDistance = (gameObject.transform.position - mPlayer.transform.position).magnitude;
+            if (playerDistance < itemDragDistance)
+            {
+                if (itemBody != null)
+                {
+                    Vector3 direction = (mPlayer.transform.position - this.transform.position).normalized;
+                    itemBody.MovePosition(transform.position + direction * itemSpeed * Time.deltaTime);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"Triggered by {other.name} (tag: {other.tag})");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player Detected!");
+            if (itemDestroyed == false) //Stops item from being able to be picked up twice!
+            {
+                //Add finger count here!
+                itemDestroyed = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
