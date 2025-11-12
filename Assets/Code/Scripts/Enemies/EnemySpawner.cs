1718ae13bd2858f9c09579f9875392350d0b47b7
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2025-11-11 16:17:23 -0800

    Fixed spawner orientation

--- Assets/Code/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Code/Scripts/Enemies/EnemySpawner.cs
@@ -48,7 +48,7 @@ public class EnemySpawner : MonoBehaviour
         Vector3 spawnPoint = new Vector3(randomWorldPos.x, terrainHeight + 1f, randomWorldPos.z);
         Vector3 decalPoint = new Vector3(randomWorldPos.x, terrainHeight + 0.1f, randomWorldPos.z);
 
-        GameObject decalInstance = Instantiate(spawnDecal, decalPoint, Quaternion.Euler(90f, 0f, -45f));
+        GameObject decalInstance = Instantiate(spawnDecal, decalPoint, Quaternion.Euler(90f, 0f, 0f));
         decalInstance.GetComponent<timedDestroy>().setDestroy(decalDestroyTime);
         StartCoroutine(spawnEnemy(spawnPoint));
     }
