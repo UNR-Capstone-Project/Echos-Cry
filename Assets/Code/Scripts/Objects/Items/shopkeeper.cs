0adc86652356412877ec5495484444c1a1197826
Abb6781 <151780295+Abb6781@users.noreply.github.com>
2025-11-11 23:58:20 -0800

    collision enables shop (hypothetically)

---
+++ Assets/Code/Scripts/Objects/Items/shopkeeper.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class shopkeeper : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject shopCanvas;
+    void Start()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.CompareTag("Player")){
+            shopCanvas.SetActive(true);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
