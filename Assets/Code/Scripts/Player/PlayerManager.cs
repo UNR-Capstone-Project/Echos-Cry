8866f4fd412cf1f7b46a62f440927fa7c728d2e0
andy-wittig <Awittig@nevada.unr.edu>
2025-11-12 17:28:16 -0800

    Scene transition fixed with player positioning!

--- Assets/Code/Scripts/Player/PlayerManager.cs
+++ Assets/Code/Scripts/Player/PlayerManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField] private PlayerStats playerStats;
+    
     void Start()
     {
         playerStats.Initialize(); //Resets stats to default values
