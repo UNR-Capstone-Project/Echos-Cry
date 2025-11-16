eb0addb5e5d51a610ec06aa4c2c740dc4ce2304a
Hyphenix <itsvictordang@gmail.com>
2025-11-15 17:21:38 -0800

    setup to ensure loading happens when loading into the level one scene, main menu functionality

--- Assets/Code/Scripts/PersistenceInitializer.cs
+++ Assets/Code/Scripts/PersistenceInitializer.cs
@@ -3,16 +3,25 @@ using UnityEngine.SceneManagement;
 
 public static class PersistenceInitializer
 {
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static bool loaded = false;
 
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     public static void Execute()
     {
+        SceneManager.sceneLoaded += LoadObjects;
+    }
+
+    static void LoadObjects(Scene scene, LoadSceneMode mode)
+    {
+        if (loaded) return;
+
         var currentScene = SceneManager.GetActiveScene();
 
         if (currentScene.name != "MainMenu")
         {
             Debug.Log("Loaded by the Persist Object from the PersistenceInitializer script");
             Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("PERSISTOBJECTS")));
+            loaded = true;
         }
         
     }
