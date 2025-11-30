61211f854679b6e7ff9101b07ff672501b65cc69
satireiguess <cgram79@yahoo.com>
2025-11-29 22:00:59 -0800

    reorganized

--- Assets/Code/Scripts/Managers/TickManager.cs
+++ Assets/Code/Scripts/Managers/TickManager.cs
@@ -3,29 +3,37 @@ using UnityEngine;
 
 public class TickManager : MonoBehaviour
 {
+    public static event Action OnTick01Event;
     public static event Action OnTick02Event;
     public static event Action OnTick05Event;
+    private float tick_timer01 = 0;
     private float tick_timer02 = 0;
     private float tick_timer05 = 0;
 
     private void Start()
     {
+        tick_timer01 = 0;
         tick_timer02 = 0;
         tick_timer05 = 0;
     }
     void Update()
     {
+        tick_timer01 += Time.deltaTime;
         tick_timer02 += Time.deltaTime;
         tick_timer05 += Time.deltaTime;
-
+        if(tick_timer01 >= 0.1f)
+        {
+            tick_timer01 -= 0.1f;
+            OnTick01Event?.Invoke();
+        }
         if (tick_timer02 >= 0.2f)
         {
-            tick_timer02 = 0;
+            tick_timer02 -= 0.2f;
             OnTick02Event?.Invoke();
         }
         if (tick_timer05 >= 0.5f)
         {
-            tick_timer05 = 0;
+            tick_timer05 -= 0.5f;
             OnTick05Event?.Invoke();
         }
     }
---
+++ Assets/Code/Scripts/UI/Player/AnimateMetronome.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class AnimateMetronome : MonoBehaviour
+{
+    [SerializeField] private GameObject pendulumObject;
+    private int swingDirection = 1;
+    private float previousProgress = 0f;
+
+    private void Update()
+    {
+        float progress = MusicManager.Instance.GetSampleProgress();
+
+        float angle = Mathf.Sin(progress * Mathf.PI) * 45f * swingDirection;
+        pendulumObject.transform.localRotation = Quaternion.Euler(0f, 0f, angle);
+
+        if (progress < previousProgress)
+        {
+            swingDirection *= -1;
+        }
+
+        previousProgress = progress;
+    }
+}
