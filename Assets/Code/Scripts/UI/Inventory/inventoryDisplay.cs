c1bcf0867fd3f4f0de1acdda4f1bc294af0db605
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-01-19 16:41:10 -0800

    Added key tool tips to slots and adjusted positioning

--- Assets/Code/Scripts/UI/Inventory/inventoryDisplay.cs
+++ Assets/Code/Scripts/UI/Inventory/inventoryDisplay.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 public class InventoryDisplay : MonoBehaviour
 {
     public slotScript[] slotScriptArray = new slotScript[4];
-    public slotScript wallet;
 
     private void OnUpdateInventory(){
         int i = 0;
         foreach(InventoryItem item in InventoryManager.Instance.inventory){
-            if(item.data.id == "finger"){
-                wallet.Set(item);
-            }else if(i == 0){
+            if(i == 0){
                 slotScriptArray[i].Set(item);
                 i++;
             }else if(i == 1){
@@ -27,19 +24,7 @@ public class InventoryDisplay : MonoBehaviour
         }
         slotScriptArray[i].Set(null);
     }
-    /*public void DrawInventory(){
-        foreach(inventoryItem item in inventoryManager.current.inventory){
-            AddInventorySlot(item);
-        }
-    }
-    public void AddInventorySlot(inventoryItem item){
-        GameObject obj = Instantiate(m_slotPrefab);
-        obj.transform.setParent(transform, false);
 
-        UIInventoryItemSlot slot = obj.GetComponent<UIInventoryItemSlot>();
-        slot.Set(item);
-    }*/
-    // Update is called once per frame
     void Update()
     {
         OnUpdateInventory();
