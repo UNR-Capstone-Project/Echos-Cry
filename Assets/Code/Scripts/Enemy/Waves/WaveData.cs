c2ac861c4569842360570d3e19d1cccaa1cc46fe
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-02-01 18:40:57 -0800

    Working on wave spawning fixes

--- Assets/Code/Scripts/Enemy/Waves/WaveData.cs
+++ Assets/Code/Scripts/Enemy/Waves/WaveData.cs
@@ -6,22 +6,20 @@ using UnityEngine;
 /// <summary>
 /// A ScriptableObject to hold all the data about enemy spawn waves to be used in an area of a level
 /// </summary>
+
+[System.Serializable]
+public struct SpawnData
+{
+    public GameObject enemySpawnType;
+    public int enemySpawnCount;
+}
+
 [CreateAssetMenu(fileName = "WaveData", menuName = "Echo's Cry/Enemy Wave System/WaveData")]
 public class WaveData : ScriptableObject
 {
-    [Tooltip("Total enemies in the wave that INCLUDES the keyed enemy.")]
-    public int totalEnemies;
-    [Tooltip("The array of all enemy types to spawn in a wave EXCLUDING the KEYED ENEMY.")]
-    public GameObject[] enemyTypesArray;
-    [Tooltip("The specific enemy that can drop a key and spawns LAST in a wave.")]
-    public GameObject keyedEnemy;
+    [Tooltip("List of every enemy type and how many will spawn in a given wave.")] 
+    public SpawnData[] EnemySpawns;
 
-    [Tooltip("The amount of each enemy type specified in enemyTypesArray to spawn, MAKE IT EQUAL TO (totalEnemies - 1)")]
-    public int[] enemySpawnCounts;
     public float spawnRadius = 10f;
-
-    [Tooltip("Delay between each enemy spawn. Shorter time like 0.1 effectively spawn the entire wave simultaneously.")]
     public float spawnInterval = 2f;   
-
-    public bool spawnCountOrderRandomized = false;
 }
