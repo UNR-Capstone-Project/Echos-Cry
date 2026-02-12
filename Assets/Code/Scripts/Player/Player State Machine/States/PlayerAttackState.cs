7a814a6e2b0ce6750f21e15a54d03a13ecfaf257
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-02-11 22:49:16 -0800

    Revert "Returned attack state back."
    This reverts commit 582db587d1624973327de6031a70f011d857c05b.

--- Assets/Code/Scripts/Player/Player State Machine/States/PlayerAttackState.cs
+++ Assets/Code/Scripts/Player/Player State Machine/States/PlayerAttackState.cs
@@ -1,32 +1,9 @@
-using System.Collections;
 using UnityEngine;
 
 public class PlayerAttackState : PlayerActionState
 {
-    Coroutine currentCoroutine;
-
     public PlayerAttackState(Player playerContext, PlayerStateMachine playerStateMachine, PlayerStateCache playerStateCache) 
         : base(playerContext, playerStateMachine, playerStateCache) { }
-
-    public override void Update()
-    {
-        //Check if the current attack is done.
-        if (_playerContext.WeaponHolder.IsActionEnded())
-        {
-            int hitCount = _playerContext.WeaponHolder.CurrentlyEquippedWeapon.HitColliders.Count;
-
-            for (int i = 0; i < hitCount; i++)
-            {
-                TempoConductor.HitQuality hitQuality = _playerContext.WeaponHolder.CurrentlyEquippedWeapon.HitColliders[i].hit;
-                _playerContext.ComboMeter.AddToComboMeter(hitQuality);
-            }
-
-            _playerStateMachine.SwitchState(_playerStateCache.RequestState(PlayerStateCache.PlayerState.Idle));
-            _playerContext.InvokeAttackEnded();
-        }
-
-        _playerContext.Animator.UpdateMainSpriteDirection(_playerStateMachine.Locomotion);
-    }
 
     public override void Enter()
     {
@@ -48,26 +25,29 @@ public class PlayerAttackState : PlayerActionState
         }
 
         _playerContext.Orientation.IsRotating = false;
-        _playerContext.Animator.SpriteAnimator.Play("Run");
-        _playerContext.SFX.Execute(_playerContext.SFXConfig.FootstepSFX, _playerContext.transform, 0);
-        currentCoroutine = _playerContext.StartCoroutine(RepeatSoundFootstep(_playerContext.SFXConfig.FootstepSFX.soundClips[0].length));
     }
+
     public override void Exit()
     {
         _playerContext.Orientation.IsRotating = true;
         _playerStateMachine.IsAttacking = false;
-
-        if (currentCoroutine != null) _playerContext.StopCoroutine(currentCoroutine);
     }
 
-    public override void FixedUpdate()
+    public override void Update()
     {
-        _playerContext.Movement.Move(_playerStateMachine.Locomotion);
-    }
-    private IEnumerator RepeatSoundFootstep(float clipLength)
-    {
-        yield return new WaitForSeconds(clipLength);
-        _playerContext.SFX.Execute(_playerContext.SFXConfig.FootstepSFX, _playerContext.transform, 0);
-        currentCoroutine = _playerContext.StartCoroutine(RepeatSoundFootstep(_playerContext.SFXConfig.FootstepSFX.soundClips[0].length));
+        //Check if the current attack is done.
+        if (_playerContext.WeaponHolder.IsActionEnded())
+        {
+            int hitCount = _playerContext.WeaponHolder.CurrentlyEquippedWeapon.HitColliders.Count;
+
+            for (int i = 0; i < hitCount; i++)
+            {
+                TempoConductor.HitQuality hitQuality = _playerContext.WeaponHolder.CurrentlyEquippedWeapon.HitColliders[i].hit;
+                _playerContext.ComboMeter.AddToComboMeter(hitQuality);
+            }
+
+            _playerStateMachine.SwitchState(_playerStateCache.RequestState(PlayerStateCache.PlayerState.Idle));
+            _playerContext.InvokeAttackEnded();
+        }
     }
 }
