a29ee5ec9f71597ab0e23faa43173eb7091b7ebe
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-02-23 11:05:56 -0800

    Simplified tempo conductor to just use percentages.

--- Assets/Code/Scripts/Managers/TempoConductor.cs
+++ Assets/Code/Scripts/Managers/TempoConductor.cs
@@ -15,78 +15,66 @@ public class TempoConductor : Singleton<TempoConductor>
     public HitQuality CurrentHitQuality { get { return _currentHitQuality; } }
 
     //Beat Timing Values
-    private float _timeBetweenBeats = 0;
-    private float _currentBeatTime = 0;
-    private float _lastProgress = 0f;
+    private float _currentBeatProgress = 0;
+    private float _timeBetweenBeats;
     
     public float TimeBetweenBeats { get { return _timeBetweenBeats; } }
 
-    //Hit Time
-    private readonly float _excellentPercent = 0.08f; 
-    private readonly float _goodPercent = 0.13f; 
+    //Hit Time --> Higher values are easier to hit within!
+    private readonly float _excellentPercent = 0.1f;
+    private readonly float _goodPercent = 0.2f;
 
     //            Tempo Threshold
     // Start                           End
     //   |--|-|-|---------------|-|-|---|
     // BEAT 1                         BEAT 2
 
-    //These HitTime measurements are used to compare the _currentBeatTime with whether it lands within the thresholds of Excellent, Good, Bad or Miss
-
-    //   ***HitTimeStart represents the time value that is compared to the currentBeatTime at the start of the tempo threshold
-    //   ***HitTimeEnd represents the time value that is compared to the currentBeatTime at the end of the tempo threshold
-
-    private float _excellentHitTime = 0;
-    private float _goodHitTime = 0;
-
     // Events
     public event Action BeatTickEvent;
 
     public bool IsOnBeat()
     {
-        //Debug.Log(CurrentHitQuality);
-        if (_currentHitQuality == HitQuality.Good || _currentHitQuality == HitQuality.Excellent) 
-            return true;
-        else
-            return false;
+        return (_currentHitQuality == HitQuality.Good || _currentHitQuality == HitQuality.Excellent);
     }
+    void Update()
+    {
+        UpdateHitQuality();
+    }
+
     private void UpdateTempo()
     {
-        //The time between each beat(60 seconds / BPM)
+        //The time between each beat: 60 seconds / BPM
         _timeBetweenBeats = 60f / (float)MusicManager.Instance.GetTempo();
-
-        _excellentHitTime = _timeBetweenBeats * _excellentPercent;
-        _goodHitTime = _timeBetweenBeats * _goodPercent;
     }
+
     private void UpdateHitQuality()
     {
-        if (_currentBeatTime > _timeBetweenBeats - _excellentHitTime || _currentBeatTime < _excellentHitTime)  
-            _currentHitQuality = HitQuality.Excellent; 
-        else if (_currentBeatTime > _timeBetweenBeats - _goodHitTime || _currentBeatTime < _goodHitTime)  
-            _currentHitQuality = HitQuality.Good; 
-        else 
+        _currentBeatProgress = MusicManager.Instance.GetSampleProgress();
+        Debug.Log(_currentBeatProgress);
+
+        if (_currentBeatProgress <= _excellentPercent ||
+            _currentBeatProgress >= (1f - _excellentPercent))
+        {
+            _currentHitQuality = HitQuality.Excellent;
+        }
+        else if (_currentBeatProgress <= _goodPercent ||
+            _currentBeatProgress >= (1f - _goodPercent))
+        {
+            _currentHitQuality = HitQuality.Good;
+        }
+        else
+        {
             _currentHitQuality = HitQuality.Miss;
+        }     
     }
 
-    void Start()
+    void OnEnable()
     {
         MusicManager.Instance.SongPlayEvent += UpdateTempo;
         UpdateTempo();
     }
-    private void OnDestroy()
+    private void OnDisable()
     {
         MusicManager.Instance.SongPlayEvent -= UpdateTempo;
-    }
-
-    void Update()
-    {
-        _currentBeatTime = MusicManager.Instance.GetSampleProgress() * _timeBetweenBeats;
-
-        if (MusicManager.Instance.GetSampleProgress() < _lastProgress)
-        {
-            BeatTickEvent?.Invoke(); //Less precise beat tick than from music manager
-        }
-        _lastProgress = MusicManager.Instance.GetSampleProgress();
-
-        UpdateHitQuality();
     }
 }
