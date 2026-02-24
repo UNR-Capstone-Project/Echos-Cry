68eaec2e93cd92ff76388dcb0c7a54344e23c7e6
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-02-24 15:49:24 -0800

    Settings menu opens from main menu.

--- Assets/Code/Scripts/UI/Menus/MainMenu.cs
+++ Assets/Code/Scripts/UI/Menus/MainMenu.cs
@@ -1,10 +1,38 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-/// Original Author: Victor
-/// All Contributors Since Creation: Victor
-/// Last Modified By:
+
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private List<StringGameobjectPair> menuDictionary;
+    [SerializeField] private SettingsManager settingsManager;
+
+    public void SetMenu(string menuName)
+    {
+        foreach (StringGameobjectPair menu in menuDictionary)
+        {
+            if (menu.key == menuName) { menu.value.SetActive(true); }
+            else { menu.value.SetActive(false); }
+        }
+    }
+
+    private void OnAwake()
+    {
+        SetMenu("Main");
+    }
+    private void OnEnable()
+    {
+        settingsManager.OnMenuBackButton += HandleBackButton;
+    }
+    private void OnDisable()
+    {
+        settingsManager.OnMenuBackButton += HandleBackButton;
+    }
+    private void HandleBackButton()
+    {
+        SetMenu("Main");
+    }
+
     public void Continue()
     {
         SceneManager.LoadScene("TownScene");
@@ -17,7 +45,7 @@ public class MainMenu : MonoBehaviour
 
     public void Settings()
     {
-        Debug.Log("Opening settings!");
+        SetMenu("Settings");
     }
 
     public void ExitGame()
