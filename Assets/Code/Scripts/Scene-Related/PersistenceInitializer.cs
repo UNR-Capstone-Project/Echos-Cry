5c5a9deedfa1d649828d74d6cc3b78fd7d9040cc
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-02-25 22:49:33 -0800

    Added credits scene and logic to end game!

--- Assets/Code/Scripts/Scene-Related/PersistenceInitializer.cs
+++ Assets/Code/Scripts/Scene-Related/PersistenceInitializer.cs
@@ -1,9 +1,11 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public static class PersistenceInitializer
 {
     private static bool loaded = false;
+    private static Object _persistenceRef;
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     public static void Execute()
@@ -13,16 +15,25 @@ public static class PersistenceInitializer
 
     static void LoadObjects(Scene scene, LoadSceneMode mode)
     {
-        if (loaded) return;
-
         var currentScene = SceneManager.GetActiveScene();
+
+        if (currentScene.name == "EndCreditsMenu")
+        {
+            if (_persistenceRef != null)
+            {
+                Object.Destroy(_persistenceRef);
+                loaded = false;
+            }
+        }
+
+        if (loaded) return;
 
         if (currentScene.name != "MainMenu" && currentScene.name != "EndCreditsMenu")
         {
             //Debug.Log("Loaded by the Persist Object from the PersistenceInitializer script");
-            Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("PERSISTOBJECTS")));
+            _persistenceRef = Object.Instantiate(Resources.Load("PERSISTOBJECTS"));
+            Object.DontDestroyOnLoad(_persistenceRef);
             loaded = true;
         }
-        
     }
 }
