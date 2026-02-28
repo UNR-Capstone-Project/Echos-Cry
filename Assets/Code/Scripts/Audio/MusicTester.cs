a5e67a2b9f7e2854dc8db99637655cf720a6e683
Colby Gram <cgram79@yahoo.com>
2026-02-27 17:00:44 -0800

    fixed bug with music and animation
    -attack animation lost override controller for some reason, fixed.
    -Music Tester needs to be renamed

---
+++ Assets/Code/Scripts/Audio/MusicTester.cs
@@ -0,0 +1,22 @@
+using SoundSystem;
+using System.Collections;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+/// Original Author: Victor
+/// All Contributors Since Creation: Victor
+/// Last Modified By:
+public class MusicTester : MonoBehaviour
+{
+    [SerializeField] private MusicEvent testSong;
+
+    void Start()
+    {
+        StartCoroutine(WaitToPlaySong(1f));
+    }
+
+    IEnumerator WaitToPlaySong(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        MusicManager.Instance.PlaySong(testSong);
+    }
+}
