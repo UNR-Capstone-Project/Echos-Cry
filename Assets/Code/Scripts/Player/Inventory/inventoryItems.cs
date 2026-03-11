8b4d960e5415e192518c88f07a81e021ccc6b7a7
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2026-03-11 00:08:34 -0700

    Added soul pickup and totem item.

--- Assets/Code/Scripts/Player/Inventory/inventoryItems.cs
+++ Assets/Code/Scripts/Player/Inventory/inventoryItems.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 public class InventoryItems : MonoBehaviour
 {
+    [SerializeField] protected GameObject particleEffect;
     public InventoryItemData itemData;
+
     public void PickupItem()
     {
         InventoryManager.Instance.Add(itemData);
+        if (particleEffect != null)
+        {
+            Instantiate(particleEffect, transform.position, Quaternion.identity);
+        }
+
         Destroy(gameObject);
     }
+
     void OnTriggerEnter(Collider other)
     {
         PickupItem();
