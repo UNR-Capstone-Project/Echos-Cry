e940ea98977cfbfe81b9b0f2e2d277c209db960c
Colby Gram <cgram79@yahoo.com>
2026-03-12 17:14:14 -0700

    init decal pool

---
+++ Assets/Code/Scripts/Core/VFX/Blood/DecalPool.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.Rendering.Universal;
+
+public class DecalPool : MonoBehaviour
+{
+    GameObject[] prefabs;
+    ObjectPool<DecalProjector> _decalPool;
+
+    
+}
